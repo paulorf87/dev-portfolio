afe71d9e91df53c02aec98c538c13e063a436dd8
Paulo Ferro <paulorf87@gmail.com>
2022-03-17 17:22:41 +1100

    Texts revised. Second last review

--- src/pages/Home/_SideBio/SideBio.jsx
+++ src/pages/Home/_SideBio/SideBio.jsx
@@ -6,9 +6,14 @@ const SideBio = () => {
     return <div className="side-bio">
         <h2>Bio</h2>
         <p>
-            Production Engineer by formation and self-taught computer programmer.
-            Started programming Human Machine Interface (HMIs) and SCADA systems in industrial automation projects.
-            Focusing on FrontEnd Development with emphasis in React JS since 2020. 
+        Being an advocate for modern engineering & developer practices, 
+        Paulo Ferro made the change from a Production Engineer to a self-taught 
+        computer programmer in the past X years. Paulo started programming Human 
+        Machine Interface (HMIs) and SCADA systems in industrial automation projects 
+        with a view to exploring new innovative procedures in the industry. 
+        Focusing on FrontEnd Development with emphasis in React JS since 2020, 
+        Paulo is currently seeking new opportunities in software development to 
+        challenge himself and have a real impact driving business outcomes for organisations. 
         </p>
         
     </div>
